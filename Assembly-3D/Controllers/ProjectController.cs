411c86c4d78515f989d1297c8c437b4903ee7c9d
cclores1 <55910085+cclores1@users.noreply.github.com>
2021-07-01 18:08:03 -0700

    LogIn>Main Page> Project page structure

---
+++ Assembly-3D/Controllers/ProjectController.cs
@@ -0,0 +1,24 @@
+﻿using Assembly_3D.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assembly_3D.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<PartsModel> partList = new List<PartsModel>();
+
+            partList.Add(new PartsModel { Id = 1, Name = "Duck", Description = "Duck" });
+            partList.Add(new PartsModel { Id = 1, Name = "Dog", Description = "Dog" });
+
+            return View(partList);
+        }
+
+      
+    }
+}
---
+++ Assembly-3D/Models/PartsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assembly_3D.Models
+{
+    public class PartsModel
+    {
+        [DisplayName("Id Number")]
+        public int Id { get; set; }
+
+        [DisplayName("Part Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Part Description")]
+        public string Description { get; set; }
+
+    }
+}
