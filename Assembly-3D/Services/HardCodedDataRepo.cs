b88143c9b6edff814b223652746dd03f8e58d43c
cclores1 <55910085+cclores1@users.noreply.github.com>
2021-07-03 12:04:22 -0700

    Changed parts list with something more relevant.

--- Assembly-3D/Services/HardCodedDataRepo.cs
+++ Assembly-3D/Services/HardCodedDataRepo.cs
@@ -21,18 +21,18 @@ namespace Assembly_3D.Services
         {
 
 
-            partList.Add(new PartsModel { Id = 0, Name = "Duck", Description = "Duck" });
-            partList.Add(new PartsModel { Id = 1, Name = "Dog", Description = "Dog" });
-            partList.Add(new PartsModel { Id = 0, Name = "Cat", Description = "Cat" });
-            partList.Add(new PartsModel { Id = 1, Name = "Bat", Description = "Bat" });
-            partList.Add(new PartsModel { Id = 0, Name = "Hamster", Description = "Hamster" });
-            partList.Add(new PartsModel { Id = 1, Name = "Tilapia", Description = "Tilapia" });
-            partList.Add(new PartsModel { Id = 0, Name = "Eagle", Description = "Eagle" });
-            partList.Add(new PartsModel { Id = 1, Name = "Squirrel", Description = "Squirrel" });
-            partList.Add(new PartsModel { Id = 0, Name = "Lamb", Description = "Lamb" });
-            partList.Add(new PartsModel { Id = 1, Name = "Sheep", Description = "Sheep" });
-            partList.Add(new PartsModel { Id = 0, Name = "Dolphin", Description = "Dolphin" });
-            partList.Add(new PartsModel { Id = 1, Name = "Tiger", Description = "Tiger" });
+            partList.Add(new PartsModel { Id = 0, Name = "Cog", Description = "Cog" });
+            partList.Add(new PartsModel { Id = 1, Name = "Wheel", Description = "Wheel" });
+            partList.Add(new PartsModel { Id = 2, Name = "Car", Description = "Car" });
+            partList.Add(new PartsModel { Id = 3, Name = "House", Description = "House" });
+            partList.Add(new PartsModel { Id = 4, Name = "Airplane", Description = "Airplane" });
+            partList.Add(new PartsModel { Id = 5, Name = "Rocket", Description = "Rocket" });
+            partList.Add(new PartsModel { Id = 6, Name = "Well", Description = "Well" });
+            partList.Add(new PartsModel { Id = 7, Name = "Shoes", Description = "Shoes" });
+            partList.Add(new PartsModel { Id = 8, Name = "Lamp", Description = "Lamp" });
+            partList.Add(new PartsModel { Id = 9, Name = "Helicopter", Description = "Helicopter" });
+            partList.Add(new PartsModel { Id = 10, Name = "3DPrinter", Description = "3DPrinter" });
+            partList.Add(new PartsModel { Id = 11, Name = "Pen", Description = "Pen" });
 
 
             return partList;
