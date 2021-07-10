67ed5de5da33e634e855ee39c2991f524d74adab
cclores1 <55910085+cclores1@users.noreply.github.com>
2021-07-10 16:43:05 -0700

    Upload button started.

---
+++ Assembly-3D/Controllers/PartsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assembly_3D.Controllers
+{
+    public class PartsController : Controller
+    {
+        private IWebHostEnvironment _env;
+        private string _dir;
+
+        public PartsController(IWebHostEnvironment env)
+        {
+            _env = env;
+            _dir = _env.ContentRootPath;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upload(IFormFile file)
+        {
+          
+
+            using (var fileStream = new FileStream(Path.Combine(_dir,"file.png"), FileMode.Create, FileAccess.Write))
+            {
+                file.CopyTo(fileStream);
+            }
+
+                return RedirectToAction("Project");
+        }
+    }
+}
